0b1f5657a8e938aaf4f6c51a309407c836925571
Not TCA <realtcatech@gmail.com>
2022-12-27 21:01:10 +0800

    add status

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,16 @@ import "dotenv/config";
 import { BotClient } from "./structures/BotClient";
 import { ActivityType } from "discord.js";
 
-const client = new BotClient();
+const client = new BotClient({
+  presence: {
+    activities: [
+      {
+        name: "the stars",
+        type: ActivityType.Watching,
+      },
+    ],
+  },
+});
 
 client.connect();
 client.register();
